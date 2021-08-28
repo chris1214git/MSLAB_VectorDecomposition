ca053c5a7de41995cdb3e80ca57bb429a8fddc52
chrisliu <r09946002@ntu.edu.tw>
2021-08-28 11:36:39 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
-wget https://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip -d ./data
-rm glove.6B.zip
-unzip data/IMDB.zip -d ./data
-
 cd data
-python3 ./get_docembedding.py
+
+wget https://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+rm glove.6B.zip
+unzip IMDB.zip
