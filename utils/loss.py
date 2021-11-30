2c845e50b5a305727c99cc1be5fed4fdd44ae8c2
chrisliu <r09946002@ntu.edu.tw>
2021-12-01 01:11:32 +0800

    modified multilabelmarginloss variable length

--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 def ListNet(y_pred, y_true, eps=1e-10):
     # (1) y_pred: the decoded vector. 
@@ -19,26 +20,6 @@ def ListNet(y_pred, y_true, eps=1e-10):
     pred_log = torch.log(pred)
 
     return torch.mean(torch.sum(-y_true * pred_log, dim=1))
-
-def listNet_origin(y_pred, y_true, eps=1e-10):
-    """
-    ListNet loss introduced in "Learning to Rank: From Pairwise Approach to Listwise Approach".
-    :param y_pred: predictions from the model, shape [batch_size, slate_length]
-    :param y_true: ground truth labels, shape [batch_size, slate_length]
-    :param eps: epsilon value, used for numerical stability
-    :param padded_value_indicator: an indicator of the y_true index containing a padded item, e.g. -1
-    :return: loss value, a torch.Tensor
-    """
-    y_pred = y_pred.clone()
-    y_true = y_true.clone()
-
-    preds_smax = F.softmax(y_pred, dim=1)
-    true_smax = F.softmax(y_true, dim=1)
-
-    preds_smax = preds_smax + eps
-    preds_log = torch.log(preds_smax)
-
-    return torch.mean(-torch.sum(true_smax * preds_log, dim=1))
 
 def topk_ListNet(y_pred, y_true, y_rank, eps=1e-10, topk=64):
     # (1) y_pred: the decoded vector. 
@@ -63,7 +44,46 @@ def topk_ListNet(y_pred, y_true, y_rank, eps=1e-10, topk=64):
     preds_log = torch.log(preds_smax)
     return torch.mean(torch.sum(-y_true * preds_log, dim=1))
 
-def listMLE(y_pred, y_true, eps=1e-10, padded_value_indicator=-1):
+def RankLoss(y_pred, y_true):
+    # (1) y_pred: the decoded vector. 
+    #     ex: tfidf score of each word in certain document.
+    # (2) y_true: the vector before encoded .
+    #     ex: same as above.
+    # warning: this loss function may cause memory limit exceeded when the size of input vector become larger.
+    y_pred = y_pred.clone()
+    y_true = y_true.clone()
+
+    return torch.relu(-(y_pred.view(-1,1)*y_pred.view(1,-1)) * (y_true.view(-1,1)*y_true.view(1,-1)))
+
+def MSERankLoss(y_pred, y_true):
+    # (1) y_pred: the decoded vector. 
+    #     ex: tfidf score of each word in certain document.
+    # (2) y_true: the vector before encoded .
+    #     ex: same as above.
+    pred_rank, rank_p = torch.sort(y_pred, dim=1, descending=True)
+    label_rank, rank_l = torch.sort(y_true, dim=1, descending=True)
+
+    return torch.nn.MSELoss().forward(torch.FloatTensor(rank_p), torch.FloatTensor(rank_l))
+
+def ListNet_origin(y_pred, y_true, eps=1e-10):
+    """
+    ListNet loss introduced in "Learning to Rank: From Pairwise Approach to Listwise Approach".
+    :param y_pred: predictions from the model, shape [batch_size, slate_length]
+    :param y_true: ground truth labels, shape [batch_size, slate_length]
+    :param eps: epsilon value, used for numerical stability
+    :param padded_value_indicator: an indicator of the y_true index containing a padded item, e.g. -1
+    :return: loss value, a torch.Tensor
+    """
+
+    preds_smax = F.softmax(y_pred, dim=1)
+    true_smax = F.softmax(y_true, dim=1)
+
+    preds_smax = preds_smax + eps
+    preds_log = torch.log(preds_smax)
+
+    return torch.mean(-torch.sum(true_smax * preds_log, dim=1))
+
+def ListMLE(y_pred, y_true, eps=1e-10, padded_value_indicator=-1):
     """
     ListMLE loss introduced in "Listwise Approach to Learning to Rank - Theory and Algorithm".
     :param y_pred: predictions from the model, shape [batch_size, slate_length]
@@ -98,38 +118,20 @@ def listMLE(y_pred, y_true, eps=1e-10, padded_value_indicator=-1):
 
     return torch.mean(torch.sum(observation_loss, dim=1))
 
-def RankLoss(y_pred, y_true):
-    # (1) y_pred: the decoded vector. 
-    #     ex: tfidf score of each word in certain document.
-    # (2) y_true: the vector before encoded .
-    #     ex: same as above.
-    # warning: this loss function may cause memory limit exceeded when the size of input vector become larger.
-    y_pred = y_pred.clone()
-    y_true = y_true.clone()
-
-    return torch.relu(-(y_pred.view(-1,1)*y_pred.view(1,-1)) * (y_true.view(-1,1)*y_true.view(1,-1)))
-
-def MSERankLoss(y_pred, y_true):
-    # (1) y_pred: the decoded vector. 
-    #     ex: tfidf score of each word in certain document.
-    # (2) y_true: the vector before encoded .
-    #     ex: same as above.
-    pred_rank, rank_p = torch.sort(y_pred, dim=1, descending=True)
-    label_rank, rank_l = torch.sort(y_true, dim=1, descending=True)
-
-    return torch.nn.MSELoss().forward(torch.FloatTensor(rank_p), torch.FloatTensor(rank_l))
-
-def MultiLabelMarginLossPos(y_pred, y_pos_id, y_neg_id, alpha=1):
+def MultiLabelMarginLossCustom(y_pred, y_true_rank, fixed_topk=50, alpha=1):
     """
     MultiLabelMarginLoss add positive pairs
     y_pos_id -> index of positive target, the same as MultiLabelMarginLoss before -1
     y_neg_id -> index of negative target
     alpha -> magnitude of positive pairs compared to negative pairs, since normally negative pairs are 100 times more than positive pairs
     """
-
+    device = y_pred.device    
+    y_pos_id = y_true_rank[:, :fixed_topk]
+    y_neg_id = y_true_rank[:, fixed_topk:]
+    
     y_pos = y_pred.gather(1, y_pos_id)
     y_neg = y_pred.gather(1, y_neg_id)
-    
+
     m = y_pos.view(y_pos.shape[0], y_pos.shape[1], 1) - y_neg.view(y_neg.shape[0], 1, y_neg.shape[1])
     m = 1 - m
     l = torch.max(m, torch.zeros(m.shape).to(device))
@@ -144,3 +146,56 @@ def MultiLabelMarginLossPos(y_pred, y_pos_id, y_neg_id, alpha=1):
     loss = (l + alpha * lp) / y_pred.shape[1] / y_pred.shape[0]
     
     return loss
+
+def MultiLabelMarginLossCustomV(y_pred, y_true_rank, y_true_topk, alpha=1):
+    """
+    variable positive candidates version
+    
+    MultiLabelMarginLoss add positive pairs
+    y_pos_id -> index of positive target, the same as MultiLabelMarginLoss before -1
+    y_neg_id -> index of negative target
+    alpha -> magnitude of positive pairs compared to negative pairs, since normally negative pairs are 100 times more than positive pairs
+    """    
+    loss = 0
+    device = y_pred.device
+    
+    y_pos_id = []
+    y_neg_id = []
+    
+    for i in range(len(y_pred)):
+        topk = min(y_true_topk[i], 100)
+        y_pos_id.append(y_true_rank[i, :topk])
+        y_neg_id.append(y_true_rank[i, topk:])
+    
+    for i in (range(y_pred.shape[0])):
+        pred, pos_id, neg_id = y_pred[i], y_pos_id[i], y_neg_id[i]
+        
+        m = pred[pos_id].view(-1, 1) - pred[neg_id].view(1, -1)
+        m = 1 - m
+        l = torch.max(m, torch.zeros(m.shape).to(device))
+        l = torch.sum(l)
+        
+        mp = pred[pos_id].view(-1, 1) - pred[pos_id].view(1, -1)
+        mp = 1 - mp - torch.eye(len(pos_id)).to(device)
+        lp = torch.max(mp, torch.zeros(mp.shape).to(device))
+        lp = torch.sum(lp)
+        loss += (l + alpha * lp) / y_pred.shape[1]
+        
+    loss /= y_pred.shape[0]
+    
+    return loss
+
+def MSE(y_pred, y_true):
+    loss = nn.MSELoss(reduction='none')(y_pred, y_true)
+    return torch.mean(torch.sum(loss, dim=1))
+
+def ListNet2(y_pred, y_true):
+    y_pred_min = torch.min(y_pred, dim=1)[0].view(-1, 1)
+    y_true_min = torch.min(y_true, dim=1)[0].view(-1, 1)
+    
+    y_pred = y_pred + y_pred_min + 10
+    y_true = y_true + y_true_min + 10
+
+    pred_log = torch.log(y_pred)
+
+    return torch.mean(torch.sum(-y_true * pred_log, dim=1))
