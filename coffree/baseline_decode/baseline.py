6222339a76706c67d73ab7116b6443228c8523fc
coffree0123 <02320013apis@gmail.com>
2021-12-07 04:05:54 +0000

    update baseline decoder and useful tool

---
+++ coffree/baseline_decode/baseline.py
@@ -0,0 +1,156 @@
+import os
+import sys
+import torch
+import argparse
+import numpy as np
+from tqdm.auto import tqdm
+from torch.utils.data import Dataset
+from collections import defaultdict
+
+sys.path.append("../..")
+
+from model import Decoder_only, Decoder_wordembed
+from utils.loss import ListNet
+from utils.data_processing import get_process_data
+from utils.eval import retrieval_normalized_dcg_all, retrieval_precision_all
+from utils.toolbox import same_seeds, get_freer_gpu, show_settings, split_data
+
+class IDEDataset(Dataset):
+    def __init__(self, 
+                 doc_vectors,
+                 doc_tfidf):
+        
+        assert len(doc_vectors) == len(doc_tfidf)
+
+        self.doc_vectors = torch.FloatTensor(doc_vectors)
+        self.doc_tfidf = torch.FloatTensor(doc_tfidf)
+        
+    def __getitem__(self, idx):
+        return self.doc_vectors[idx], self.doc_tfidf[idx]
+
+    def __len__(self):
+        return len(self.doc_vectors)
+
+def evaluate_Decoder(config, model, data_loader, device):
+    results = defaultdict(list)
+    model.eval()
+        
+    # predict all data
+    for data in data_loader:
+        doc_embs, target = data
+        
+        doc_embs = doc_embs.to(device)
+        target = target.to(device)
+                
+        pred = model(doc_embs)
+    
+        # Precision
+        precision_scores = retrieval_precision_all(pred, target, k=config["topk"])
+        for k, v in precision_scores.items():
+            results['precision@{}'.format(k)].append(v)
+        
+        # NDCG
+        ndcg_scores = retrieval_normalized_dcg_all(pred, target, k=config["topk"])
+        for k, v in ndcg_scores.items():
+            results['ndcg@{}'.format(k)].append(v)
+        
+    for k in results:
+        results[k] = np.mean(results[k])
+
+    return results
+
+def train_model(decoder, device, config, train_loader, valid_loader):
+    # early stop settings 
+    stop_rounds = 3
+    no_improvement = 0
+    best_score = None
+    optimizer = torch.optim.Adam(decoder.parameters(), lr = config["lr"])
+
+    for epoch in range(config["epochs"]):
+        # Training
+        decoder.train()
+        train_loss = []
+        for batch in tqdm(train_loader, desc="Training"):
+            batch = [i.to(device) for i in batch]
+            doc_embs, target = batch
+            target = torch.nn.functional.normalize(target.to(device), dim=1)
+            decoded = torch.nn.functional.normalize(decoder(doc_embs), dim=1)
+            loss = ListNet(decoded, target)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            train_loss.append(loss.item())
+
+        print(f"[Epoch {epoch+1:02d}]")
+        res = evaluate_Decoder(config, decoder, valid_loader, device)
+        for key,val in res.items():
+            print(f"{key}:{val:.4f}")
+            
+        # early stopping 
+        current_score = res["precision@10"]
+        if best_score == None:
+            best_score = current_score
+            continue
+        if current_score < best_score:
+            no_improvement += 1
+        if no_improvement >= stop_rounds:
+            print("Early stopping...")
+            break 
+        if current_score > best_score:
+            no_improvement = 0
+            best_score = current_score
+
+    return decoder
+
+
+def main():
+    parser = argparse.ArgumentParser(description='document decomposition.')
+    parser.add_argument('--dataset', type=str, default="IMDB")
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--seed', type=int, default=123)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--hidden_dim', type=int, default=1024)
+    parser.add_argument('--decoder', type=str, default="Only")  # (1). Only: directly docode (2). wordembed: use wording embedding information
+    parser.add_argument('--topk', type=int, nargs='+', default=[10, 30, 50])
+    args = parser.parse_args()
+    config = vars(args)
+
+    show_settings(config)
+
+    same_seeds(config["seed"])
+
+    device = "cuda:{}".format(get_freer_gpu())  # Use the gpu with the most vram.
+
+    data_dict = get_process_data(config["dataset"])
+
+    importance_score = data_dict["document_word_weight"]
+    docvec = np.load("../../data/docvec_IMDB_SBERT_768d.npy")
+
+    print("TFIDF dim:{}".format(importance_score.shape))
+    print("Doc vector shape:{}".format(docvec.shape))
+
+    dim = 768
+    vocab_size = importance_score.shape[1]
+    print("Vocab size:{}".format(vocab_size))
+
+    dataset = IDEDataset(docvec, importance_score)
+
+    train_loader, valid_loader, test_loader = split_data(dataset, config)
+
+    if config["decoder"] == 'wordembed':
+        decoder = Decoder_wordembed(input_dim=dim, hidden_dim=config["hidden_dim"], output_dim=vocab_size).to(device)
+        decoder.init_weights()
+    else:
+        decoder = Decoder_only(input_dim=dim, output_dim=vocab_size).to(device)
+
+    decoder = train_model(decoder, device, config, train_loader, valid_loader)
+
+    print("Testing...")
+    res = evaluate_Decoder(config, decoder, test_loader, device)
+    for key,val in res.items():
+        print(f"{key}:{val:.4f}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/toolbox.py
@@ -0,0 +1,52 @@
+import os
+import torch
+import numpy as np
+from torch.utils.data import DataLoader, random_split
+
+
+def get_freer_gpu():
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free > tmp')
+    memory_available = [int(x.split()[2])
+                        for x in open('tmp', 'r').readlines()]
+    os.system('rm -f tmp')
+    return int(np.argmax(memory_available))
+
+
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def show_settings(config):
+    print('-------- Info ---------')
+    settings = ""
+    for key in list(config.keys()):
+        settings += "{}: {}\n".format(key, config.get(key))
+    print(settings)
+    print('-----------------------')
+
+def split_data(dataset, config):
+    train_length = int(len(dataset)*0.6)
+    valid_length = int(len(dataset)*0.2)
+    test_length = len(dataset) - train_length - valid_length
+
+    train_dataset, valid_dataset, test_dataset = random_split(
+        dataset, lengths=[train_length, valid_length,test_length],
+        generator=torch.Generator().manual_seed(42)
+        )
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=config["batch_size"], 
+        shuffle=True, pin_memory=True,
+    )
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=config["batch_size"], shuffle=False, pin_memory=True)
+    test_loader = DataLoader(
+        test_dataset, batch_size=config["batch_size"], shuffle=False)
+
+    return train_loader, valid_loader, test_loader
