5c5382e9d7770045668f603c9da1d71bddf3ca6f
ccasimir <52626458+ccasimir@users.noreply.github.com>
2022-03-01 14:58:01 +0800

    Add files via upload

---
+++ wayne/ztm_modified_file/data_preparation.py
@@ -0,0 +1,155 @@
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import scipy.sparse
+import warnings
+from contextualized_topic_models.datasets.dataset import CTMDataset
+### casimir
+# (1) import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+###
+from sklearn.preprocessing import OneHotEncoder
+
+def get_bag_of_words(data, min_length):
+    """
+    Creates the bag of words
+    """
+    vect = [np.bincount(x[x != np.array(None)].astype('int'), minlength=min_length)
+            for x in data if np.sum(x[x != np.array(None)]) != 0]
+
+    vect = scipy.sparse.csr_matrix(vect)
+    return vect
+
+
+def bert_embeddings_from_file(text_file, sbert_model_to_load, batch_size=200):
+    """
+    Creates SBERT Embeddings from an input file
+    """
+    model = SentenceTransformer(sbert_model_to_load)
+    with open(text_file, encoding="utf-8") as filino:
+        train_text = list(map(lambda x: x, filino.readlines()))
+
+    return np.array(model.encode(train_text, show_progress_bar=True, batch_size=batch_size))
+
+
+def bert_embeddings_from_list(texts, sbert_model_to_load, batch_size=200):
+    """
+    Creates SBERT Embeddings from a list
+    """
+    model = SentenceTransformer(sbert_model_to_load)
+    return np.array(model.encode(texts, show_progress_bar=True, batch_size=batch_size))
+
+
+class TopicModelDataPreparation:
+
+    def __init__(self, contextualized_model=None, show_warning=True, target='BoW'):
+        self.contextualized_model = contextualized_model
+        self.vocab = []
+        self.id2token = {}
+        self.vectorizer = None
+        self.label_encoder = None
+        self.show_warning = show_warning
+        self.target = target
+
+    def load(self, contextualized_embeddings, bow_embeddings, id2token, labels=None):
+        return CTMDataset(contextualized_embeddings, bow_embeddings, id2token, labels)
+
+    def fit(self, text_for_contextual, text_for_bow, labels=None, custom_embeddings=None):
+        """
+        This method fits the vectorizer and gets the embeddings from the contextual model
+
+        :param text_for_contextual: list of unpreprocessed documents to generate the contextualized embeddings
+        :param text_for_bow: list of preprocessed documents for creating the bag-of-words
+        :param custom_embeddings: np.ndarray type object to use custom embeddings (optional).
+        :param labels: list of labels associated with each document (optional).
+        """
+
+        if custom_embeddings is not None:
+            assert len(text_for_contextual) == len(custom_embeddings)
+
+            if text_for_bow is not None:
+                assert len(custom_embeddings) == len(text_for_bow)
+
+        if text_for_bow is not None:
+            assert len(text_for_contextual) == len(text_for_bow)
+
+        if self.contextualized_model is None and custom_embeddings is None:
+            raise Exception("A contextualized model or contextualized embeddings must be defined")
+
+        if custom_embeddings and type(custom_embeddings).__module__ != 'numpy':
+            raise TypeError("contextualized_embeddings must be a numpy.ndarray type object")
+
+        ### casimir
+        # (1) Add two different vectorizer (Tfidf, Count with binary True)
+        if self.target == 'tfidf':
+            self.vectorizer = TfidfVectorizer()
+        elif self.target == 'BoW_01':
+            self.vectorizer = CountVectorizer(binary=True)
+        else:
+            self.vectorizer = CountVectorizer()
+        ###
+        train_bow_embeddings = self.vectorizer.fit_transform(text_for_bow)
+        if custom_embeddings is None:
+            train_contextualized_embeddings = bert_embeddings_from_list(text_for_contextual, self.contextualized_model)
+        else:
+            train_contextualized_embeddings = custom_embeddings
+        self.vocab = self.vectorizer.get_feature_names()
+        self.id2token = {k: v for k, v in zip(range(0, len(self.vocab)), self.vocab)}
+
+        if labels:
+            self.label_encoder = OneHotEncoder()
+            encoded_labels = self.label_encoder.fit_transform(np.array([labels]).reshape(-1, 1))
+        else:
+            encoded_labels = None
+
+        ### casimir
+        # (1) Add one passing parameter (text_for_contexual). text_for_contextual is the list of raw documents
+        return CTMDataset(train_contextualized_embeddings, train_bow_embeddings, text_for_contextual, self.id2token, encoded_labels)
+        ###
+    def transform(self, text_for_contextual, text_for_bow=None, custom_embeddings=None, labels=None):
+        """
+        This method create the input for the prediction. Essentially, it creates the embeddings with the contextualized
+        model of choice and with trained vectorizer.
+
+        If text_for_bow is missing, it should be because we are using ZeroShotTM
+
+        :param text_for_contextual: list of unpreprocessed documents to generate the contextualized embeddings
+        :param text_for_bow: list of preprocessed documents for creating the bag-of-words
+        :param custom_embeddings: np.ndarray type object to use custom embeddings (optional).
+        :param labels: list of labels associated with each document (optional).
+        """
+
+        if custom_embeddings is not None:
+            assert len(text_for_contextual) == len(custom_embeddings)
+
+            if text_for_bow is not None:
+                assert len(custom_embeddings) == len(text_for_bow)
+
+        if text_for_bow is not None:
+            assert len(text_for_contextual) == len(text_for_bow)
+
+        if self.contextualized_model is None:
+            raise Exception("You should define a contextualized model if you want to create the embeddings")
+
+        if text_for_bow is not None:
+            test_bow_embeddings = self.vectorizer.transform(text_for_bow)
+        else:
+            # dummy matrix
+            if self.show_warning:
+                warnings.simplefilter('always', DeprecationWarning)
+                warnings.warn("The method did not have in input the text_for_bow parameter. This IS EXPECTED if you "
+                          "are using ZeroShotTM in a cross-lingual setting")
+
+            # we just need an object that is matrix-like so that pytorch does not complain
+            test_bow_embeddings = scipy.sparse.csr_matrix(np.zeros((len(text_for_contextual), 1)))
+
+        if custom_embeddings is None:
+            test_contextualized_embeddings = bert_embeddings_from_list(text_for_contextual, self.contextualized_model)
+        else:
+            test_contextualized_embeddings = custom_embeddings
+
+        if labels:
+            encoded_labels = self.label_encoder.transform(np.array([labels]).reshape(-1, 1))
+        else:
+            encoded_labels = None
+
+        return CTMDataset(test_contextualized_embeddings, test_bow_embeddings, self.id2token, encoded_labels)
