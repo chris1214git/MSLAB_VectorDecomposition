a8bf0dc90fa7c0e2e0d40122bfae16119cea01ea
ccasimir <chrischen0304@gmail.com>
2022-04-27 10:27:57 +0000

    KNN update

--- coffree/baseline/knn_baseline.py
+++ coffree/baseline/knn_baseline.py
@@ -10,17 +10,22 @@ import torch
 from torch.utils.data import random_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
-from utils.eval import retrieval_normalized_dcg_all, retrieval_precision_all
+from utils.eval import retrieval_normalized_dcg_all, retrieval_precision_all, semantic_precision_all
 from utils.toolbox import same_seeds, show_settings, record_settings, get_preprocess_document, get_preprocess_document_embs, get_preprocess_document_labels, get_word_embs
 
 torch.set_num_threads(8)
 
-def evaluate_Decoder(config, model, X_test, y_test):
+def evaluate_Decoder(config, model, vocabulary, word_embeddings, X_test, y_test):
     results = defaultdict(list)
         
     # predict all data
     pred = torch.tensor(model.predict(X_test))
     y = torch.tensor(y_test)
+
+    # Semantic Prcision
+    precision_scores, word_result = semantic_precision_all(pred, y, word_embeddings, vocabulary, k=config['topk'], th=config['threshold'])
+    for k, v in precision_scores.items():
+        results['Semantic Precision@{}'.format(k)].append(v)
 
     # Precision
     precision_scores = retrieval_precision_all(pred, y, k=config["topk"])
@@ -39,11 +44,11 @@ def evaluate_Decoder(config, model, X_test, y_test):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='document decomposition.')
-    parser.add_argument('--model', type=str, default="ZTM")
+    parser.add_argument('--model', type=str, default="KNN")
     parser.add_argument('--dataset', type=str, default="20news")
     parser.add_argument('--min_df', type=int, default=1)
-    parser.add_argument('--mxa_df', type=float, default=1.0)
-    parser.add_argument('--vocabulary_size', type=int, default=8000)
+    parser.add_argument('--max_df', type=float, default=1.0)
+    parser.add_argument('--vocab_size', type=int, default=0)
     parser.add_argument('--min_doc_word', type=int, default=15)
     parser.add_argument('--encoder', type=str, default='roberta')
     parser.add_argument('--target', type=str, default='tf-idf')
@@ -57,11 +62,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     config = vars(args)
-    config["dataset_name"] = config["dataset"]
-
-    show_settings(config)
-    record_settings(config)
     same_seeds(config["seed"])
+
+    # Parameter
+    if config['dataset'] == '20news':
+        config['min_df'], config['max_df'], config['min_doc_word'] = 50, 1.0, 15
+    elif config['dataset'] == 'agnews':
+        config['min_df'], config['max_df'], config['min_doc_word'] = 100, 1.0, 15
+    elif config['dataset'] == 'tweet':
+        config['min_df'], config['max_df'], config['min_doc_word'] = 5, 1.0, 15
     
     # data preprocessing
     unpreprocessed_corpus ,preprocessed_corpus = get_preprocess_document(**config)
@@ -73,6 +82,14 @@ if __name__ == '__main__':
     # Decode target & Vocabulary
     labels, vocabularys= get_preprocess_document_labels(preprocessed_corpus)
 
+    # word embedding preparation
+    word_embeddings = get_word_embs(vocabularys[config['target']], id2token=id2token, data_type='tensor')
+
+    # show setting
+    config['vocab_size'] = len(vocabularys[config['target']])
+    show_settings(config)
+    record_settings(config)
+
     X_train, X_test, y_train, y_test = train_test_split(doc_embs, labels[config["target"]], test_size=0.2, random_state=config["seed"])
 
     X_train, X_test, y_train, y_test = train_test_split(doc_embs, labels[config["target"]], test_size=0.2, random_state=config["seed"])
@@ -81,7 +98,9 @@ if __name__ == '__main__':
 
     model.fit(X_train, y_train)
 
-    res = evaluate_Decoder(config, model, X_test, y_test)
+    res = evaluate_Decoder(config, model, vocabularys[config['target']], word_embeddings, X_test, y_test)
 
+    record = open('./'+config['dataset']+'_'+config['model']+'_'+config['encoder']+'_'+config['target']+'.txt', 'a')
     for key,val in res.items():
         print(f"{key}:{val:.4f}")
+        record.write(f"{key}:{val:.4f}\n")
