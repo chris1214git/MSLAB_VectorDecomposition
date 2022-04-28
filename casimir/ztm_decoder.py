9e936e8c9783cc70c1620ccef1e0da0cd5063a76
ccasimir <chrischen0304@gmail.com>
2022-04-28 13:14:45 +0000

    update

--- casimir/ztm_decoder.py
+++ casimir/ztm_decoder.py
@@ -24,7 +24,7 @@ if __name__ =='__main__':
     parser.add_argument('--model', type=str, default="ZTM")
     parser.add_argument('--activation', type=str, default="sigmoid")
     parser.add_argument('--dataset', type=str, default="20news")
-    parser.add_argument('--use_pos', type=bool, default=False)
+    parser.add_argument('--use_pos', type=bool, default=True)
     parser.add_argument('--min_df', type=int, default=1)
     parser.add_argument('--max_df', type=float, default=1.0)
     parser.add_argument('--vocab_size', type=int, default=0)
