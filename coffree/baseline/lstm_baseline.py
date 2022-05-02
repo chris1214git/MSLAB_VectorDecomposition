b731cfde32c73adfc6328dbd1735a88527caa5de
coffree0123 <02320013apis@gmail.com>
2022-05-02 07:40:01 +0000

    update lstm_baseline

---
+++ coffree/baseline/lstm_baseline.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append("../..")
+
+import torch
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+
+from model import Encoder, Decoder
+from utils.toolbox import same_seeds
+
+
+def train(model, iterator, optimizer, criterion, clip):
+
+    model.train()
+
+    epoch_loss = 0
+
+    for i, batch in enumerate(iterator):
+
+        src = batch.src
+        trg = batch.trg
+
+        optimizer.zero_grad()
+
+        output = model(src, trg)
+
+        # trg = [trg len, batch size]
+        # output = [trg len, batch size, output dim]
+
+        output_dim = output.shape[-1]
+
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+
+        # trg = [(trg len - 1) * batch size]
+        # output = [(trg len - 1) * batch size, output dim]
+
+        loss = criterion(output, trg)
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+
+if __name__ == '__main__':
+    same_seeds(123)
+    enc = Encoder()
